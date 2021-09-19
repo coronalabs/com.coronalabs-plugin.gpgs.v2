6b2796fed1c15d9409ed484a41ab4d7b9494d6f9
Scott Harrison <scottrules44@gmail.com>
2021-09-18 20:33:17 -0400

    Change version requirements
    Change version requirements

--- plugins/2018.3326/android/metadata.lua
+++ plugins/2018.3326/android/metadata.lua
@@ -2,6 +2,11 @@ local metadata = {
 	plugin = {
 		format = "jar"
 	},
+	coronaManifest = {
+		dependencies = {
+			["shared.google.play.services.games"] = "com.coronalabs"
+		}
+	}
 }
 
 return metadata
